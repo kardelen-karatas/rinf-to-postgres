bd6d4c56b835501c597ded440261eda603e17297
kardelen-karatas <kardelen.karatas@gmail.com>
2018-01-08 16:24:16 +0100

    location_point and parameter tables

--- rinf_xml_to_postgres.py
+++ rinf_xml_to_postgres.py
@@ -10,8 +10,17 @@ def process_ms(ms):
     MEMBER_STATES_OUTPUT_FILE.write(MS_SQL_TEMPLATE % (MEM_code, MEM_version))
 
 
+def process_railway_location(railway_location):
+    global OPP_uniqueid
+    global LIN_line
+    RAL_distance = railway_location.get ("Kilometer").replace(",", ".")
+    RAL_natid = railway_location.get ("NationalIdentNum")
+    RAILWAY_LOCATION_OUTPUT_FILE.write (RAILWAY_LOCATION_SQL_TEMPLATE % (RAL_distance, RAL_natid,OPP_uniqueid,LIN_line))
+
+
 def process_sol(sol):
     global MEM_member
+    global LIN_line
     SOL_imcode = sol.find("SOLIMCode").get("Value")
     SOL_length = sol.find("SOLLength").get("Value").replace(",", ".")
     SOL_nature = sol.find("SOLNature").get("OptionalValue")
@@ -21,6 +30,7 @@ def process_sol(sol):
     SECTION_OF_LINE_OUTPUT_FILE.write(SOL_SQL_TEMPLATE % (
         SOL_length, SOL_nature, SOL_imcode, OPP_start, OPP_end, MEM_member, LIN_line))
 
+
     EXI_start = sol.get("ValidityDateStart")
     EXI_end = sol.get("ValidityDateEnd")
     if EXI_start or EXI_end is not None:
@@ -28,8 +38,10 @@ def process_sol(sol):
             VALIDITY_DATE_SQL_TEMPLATE % (EXI_start, EXI_end))
 
 
+
 def process_op(op):
     global MEM_member
+    global OPP_uniqueid
     OPP_uniqueid = op.find('UniqueOPID').get("Value")
     OPP_name = op.find('OPName').get("Value").replace("'", "''")
     OPP_taftapcode = op.find('OPTafTapCode').get("Value")
@@ -39,6 +51,7 @@ def process_op(op):
     OPERATIONAL_POINT_OUTPUT_FILE.write(OP_SQL_TEMPLATE % (
         OPP_name, OPP_uniqueid, OPP_lon, OPP_lat, OPP_taftapcode, MEM_member))
 
+
     EXI_start = op.get("ValidityDateStart")
     EXI_end = op.get("ValidityDateEnd")
     if EXI_start or EXI_end is not None:
@@ -47,16 +60,27 @@ def process_op(op):
 
 
 def process_op_track(op_track):
+    global OPP_uniqueid
     OTR_name = op_track.find('OPTrackIdentification').get(
         "Value").replace("'", "''")
     OTR_imcode = op_track.find('OPTrackIMCode').get("Value")
-    OP_TRACK_OUTPUT_FILE.write(OP_TRACK_SQL_TEMPLATE % (OTR_name, OTR_imcode))
+    OP_TRACK_OUTPUT_FILE.write(OP_TRACK_SQL_TEMPLATE % (OTR_name, OTR_imcode, OPP_uniqueid))
 
+    #VALIDITY DATE
     EXI_start = op_track.get("ValidityDateStart")
     EXI_end = op_track.get("ValidityDateEnd")
     if EXI_start or EXI_end is not None:
         VALIDITY_DATE_OUTPUT_FILE.write(
             VALIDITY_DATE_SQL_TEMPLATE % (EXI_start, EXI_end))
+
+    #PARAMETERS
+    PAR_name = op_track.find('OPTrackParameter').get("ID")
+    PAR_value = op_track.find('OPTrackParameter').get("Value")
+    PAR_opvalue = op_track.find('OPTrackParameter').get("OptionalValue")
+    ISA_isapplicable = op_track.find('OPTrackParameter').get("IsApplicable")
+    TCA_trackcat = 'OP_TRACK'
+    PARAMETER_OUTPUT_FILE.write(PARAMETER_SQL_TEMPLATE % (PAR_name, PAR_value, PAR_opvalue,ISA_isapplicable,TCA_trackcat))
+
 
 
 def process_op_tunnel(op_tunnel):
@@ -66,11 +90,13 @@ def process_op_tunnel(op_tunnel):
     OP_TUNNEL_OUTPUT_FILE.write(OP_TUNNEL_SQL_TEMPLATE %
                                 (OTU_name, OTU_imcode))
 
+    #VALIDITY DATE
     EXI_start = op_tunnel.get("ValidityDateStart")
     EXI_end = op_tunnel.get("ValidityDateEnd")
     if EXI_start or EXI_end is not None:
         VALIDITY_DATE_OUTPUT_FILE.write(
             VALIDITY_DATE_SQL_TEMPLATE % (EXI_start, EXI_end))
+
 
 
 def process_op_platform(op_platform):
@@ -80,11 +106,21 @@ def process_op_platform(op_platform):
     OP_PLATFORM_OUTPUT_FILE.write(
         OP_PLATFORM_SQL_TEMPLATE % (OPL_name, OPL_imcode))
 
+    #VALIDITY DATE
     EXI_start = op_platform.get("ValidityDateStart")
     EXI_end = op_platform.get("ValidityDateEnd")
     if EXI_start or EXI_end is not None:
         VALIDITY_DATE_OUTPUT_FILE.write(
             VALIDITY_DATE_SQL_TEMPLATE % (EXI_start, EXI_end))
+
+
+    #PARAMETER
+    PAR_name = op_platform.find('OPTrackPlatformParameter').get("ID")
+    PAR_value = op_platform.find('OPTrackPlatformParameter').get("Value")
+    PAR_opvalue = op_platform.find('OPTrackPlatformParameter').get("OptionalValue")
+    ISA_isapplicable = op_platform.find('OPTrackPlatformParameter').get("IsApplicable")
+    TCA_trackcat = 'OP_PLATFORM'
+    PARAMETER_OUTPUT_FILE.write(PARAMETER_SQL_TEMPLATE % (PAR_name, PAR_value, PAR_opvalue,ISA_isapplicable,TCA_trackcat))
 
 
 def process_sol_track(sol_track):
@@ -94,11 +130,23 @@ def process_sol_track(sol_track):
     SOL_TRACK_OUTPUT_FILE.write(SOL_TRACK_SQL_TEMPLATE %
                                 (STR_name, STR_direction))
 
+
+    #VALIDITY DATE
     EXI_start = sol_track.get("ValidityDateStart")
     EXI_end = sol_track.get("ValidityDateEnd")
     if EXI_start or EXI_end is not None:
         VALIDITY_DATE_OUTPUT_FILE.write(
             VALIDITY_DATE_SQL_TEMPLATE % (EXI_start, EXI_end))
+
+
+    #PARAMETER
+    PAR_name = sol_track.find('SOLTrackParameter').get("ID")
+    PAR_value = sol_track.find('SOLTrackParameter').get("Value")
+    PAR_opvalue = sol_track.find('SOLTrackParameter').get("OptionalValue")
+    ISA_isapplicable = sol_track.find('SOLTrackParameter').get("IsApplicable")
+    TCA_trackcat = 'SOL_TRACK'
+    PARAMETER_OUTPUT_FILE.write(PARAMETER_SQL_TEMPLATE % (PAR_name, PAR_value, PAR_opvalue,ISA_isapplicable,TCA_trackcat))
+
 
 
 def process_sol_tunnel(sol_tunnel):
@@ -116,11 +164,23 @@ def process_sol_tunnel(sol_tunnel):
     SOL_TUNNEL_OUTPUT_FILE.write(SOL_TUNNEL_SQL_TEMPLATE % (
         STU_name, STU_startlon, STU_startlat, STU_endlon, STU_endlat, STU_imcode))
 
+
+    #VALIDITY DATE
     EXI_start = sol_tunnel.get("ValidityDateStart")
     EXI_end = sol_tunnel.get("ValidityDateEnd")
     if EXI_start or EXI_end is not None:
         VALIDITY_DATE_OUTPUT_FILE.write(
             VALIDITY_DATE_SQL_TEMPLATE % (EXI_start, EXI_end))
+
+
+
+def process_location_point(location_point):
+    LOC_lon = location_point.get('Longitude').replace(",", ".")
+    LOC_lon = location_point.get('Latitude').replace(",", ".")
+    LOC_distance = location_point.get('Kilometer').replace(",", ".")
+    LOCATION_POINT_OUTPUT_FILE.write(LOCATION_POINT_SQL_TEMPLATE %
+                                (LOC_lon, LOC_lon,LOC_distance))
+
 
 
 def fast_iter(context):
@@ -157,6 +217,14 @@ def fast_iter(context):
             if event == 'end':
                 process_sol_tunnel(elem)
                 elem.clear()
+        elif elem.tag == 'OPRailwayLocation':
+            if event == 'end':
+                process_railway_location(elem)
+                elem.clear()
+        elif elem.tag == 'LocationPoint':
+            if event == 'end':
+                process_location_point(elem)
+                elem.clear()
 
     del context
 
@@ -164,15 +232,21 @@ def fast_iter(context):
 MS_SQL_TEMPLATE = "INSERT INTO MEMBER (MEM_code,MEM_version) VALUES ('%s','%s');\n"
 SOL_SQL_TEMPLATE = "INSERT INTO SECTION_OF_LINE (SOL_length, SOL_nature, SOL_imcode, OPP_start, OPP_end, MEM_member, LIN_line) VALUES ('%s','%s','%s','%s','%s','%s','%s');\n"
 OP_SQL_TEMPLATE = "INSERT INTO OPERATIONAL_POINT (OPP_name, OPP_uniqueid, OPP_lon, OPP_lat, OPP_taftapcode, MEM_member) VALUES ('%s','%s','%s','%s','%s','%s');\n"
-OP_TRACK_SQL_TEMPLATE = "INSERT INTO OP_TRACK (OTR_name,OTR_imcode) VALUES ('%s','%s');\n"
+OP_TRACK_SQL_TEMPLATE = "INSERT INTO OP_TRACK (OTR_name,OTR_imcode,OPP_uniqueid) VALUES ('%s','%s','%s');\n"
 OP_TUNNEL_SQL_TEMPLATE = "INSERT INTO OP_TUNNEL (OTU_name,OTU_imcode) VALUES ('%s','%s');\n"
 OP_PLATFORM_SQL_TEMPLATE = "INSERT INTO OP_PLATFORM (OPL_name,OPL_imcode) VALUES ('%s','%s');\n"
 SOL_TRACK_SQL_TEMPLATE = "INSERT INTO SOL_TRACK (STR_name,STR_direction) VALUES ('%s','%s');\n"
 SOL_TUNNEL_SQL_TEMPLATE = "INSERT INTO SOL_TUNNEL (STU_name,STU_startlon,STU_startlat,STU_endlon,STU_endlat,STU_imcode) VALUES ('%s','%s','%s','%s','%s','%s');\n"
 VALIDITY_DATE_SQL_TEMPLATE = "INSERT INTO EXISTENCE (EXI_start,EXI_end) VALUES ('%s','%s');\n"
+RAILWAY_LOCATION_SQL_TEMPLATE = "INSERT INTO RAILWAY_LOCATION (RAL_distance,RAL_natid,OPP_uniqueid,LIN_line) VALUES ('%s','%s','%s','%s');\n"
+LOCATION_POINT_SQL_TEMPLATE = "INSERT INTO LOCATION_POINT (LOC_lon, LOC_lon,LOC_distance) VALUES ('%s','%s','%s');\n"
+PARAMETER_SQL_TEMPLATE = "INSERT INTO PARAMETER (PAR_name, PAR_value,PAR_opvalue, ISA_isapplicable, TCA_trackcat) VALUES ('%s','%s','%s','%s','%s');\n"
 
 
 MEM_member = ''
+OPP_uniqueid = ''
+LIN_line = ''
+
 
 parser = argparse.ArgumentParser(
     description="Extracts data from rinf xml file and outputs sql statements in separate files")
@@ -204,11 +278,22 @@ OP_PLATFORM_OUTPUT_COUNT = 0
 MEMBER_STATES_OUTPUT_FILE = open('member_states.sql', 'w')
 MEMBER_STATES_OUTPUT_COUNT = 0
 
+RAILWAY_LOCATION_OUTPUT_FILE = open('railway_location.sql', 'w')
+RAILWAY_LOCATION_OUTPUT_COUNT = 0
+
 VALIDITY_DATE_OUTPUT_FILE = open('validity_date.sql', 'w')
 VALIDITY_DATE_OUTPUT_COUNT = 0
 
+LOCATION_POINT_OUTPUT_FILE = open('location_point.sql','w')
+LOCATION_POINT_OUTPUT_FILE_COUNT = 0
+
+PARAMETER_OUTPUT_FILE = open('parameter.sql','w')
+PARAMETER_OUTPUT_FILE_COUNT = 0
+
+
 context = etree.iterparse(arguments.rinf)
 fast_iter(context)
+
 
 SECTION_OF_LINE_OUTPUT_FILE.close()
 SOL_TRACK_OUTPUT_FILE.close()
@@ -219,3 +304,5 @@ OP_TUNNEL_OUTPUT_FILE.close()
 OP_PLATFORM_OUTPUT_FILE.close()
 MEMBER_STATES_OUTPUT_FILE.close()
 VALIDITY_DATE_OUTPUT_FILE.close()
+RAILWAY_LOCATION_OUTPUT_FILE.close()
+PARAMETER_OUTPUT_FILE.close()
--- sql/create.sql
+++ sql/create.sql
@@ -3,17 +3,17 @@ CREATE TABLE IF NOT EXISTS MEMBER (
 	MEM_version varchar
 );
 
-/*
+
 CREATE TABLE RAILWAY_LOCATION (
-	RAL_id serial ,
+	--RAL_id serial ,
 	RAL_distance float8 ,
 	RAL_natid varchar ,
-	OPP_op bigint ,
+	OPP_uniqueid bigint ,
 	LIN_line bigint 
 );
 
 
-
+/*
 CREATE TABLE OP_TYPE (
 	OTY_id serial ,
 	OTY_value varchar ,
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS OPERATIONAL_POINT (
 	OPP_lon float8 ,
 	OPP_lat float8 ,
 	OPP_taftapcode varchar ,
-    	--EXI_date bigint
+  --EXI_date bigint
 	--OTY_type bigint ,
 	MEM_member varchar
 );
@@ -51,8 +51,8 @@ CREATE TABLE IF NOT EXISTS OPERATIONAL_POINT (
 
 CREATE TABLE IF NOT EXISTS OP_TRACK (
 	--OTR_id serial ,
-    	--EXI_date bigint,
-    	--OPP_op varchar, 
+  --EXI_date bigint,
+  OPP_uniqueid varchar,
 	OTR_name varchar ,
 	OTR_imcode int
 
@@ -63,8 +63,8 @@ CREATE TABLE IF NOT EXISTS OP_TRACK (
 
 CREATE TABLE IF NOT EXISTS OP_TUNNEL (
 	--OTU_id serial ,
-    	--EXI_date bigint ,
-    	--OTR_optrack varchar , 
+  --EXI_date bigint ,
+  --OTR_optrack varchar ,
 	OTU_name varchar ,
 	OTU_imcode int
 	
@@ -74,8 +74,8 @@ CREATE TABLE IF NOT EXISTS OP_TUNNEL (
 
 CREATE TABLE IF NOT EXISTS OP_PLATFORM (
 	--OPL_id serial ,
-    	--EXI_date bigint ,
-    	--OTR_optrack bigint ,
+  --EXI_date bigint ,
+  --OTR_optrack bigint ,
 	OPL_name varchar ,
 	OPL_IMCode int
 
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS OP_PLATFORM (
 
 CREATE TABLE IF NOT EXISTS SOL_TRACK (
 	--STR_id serial ,
-    	--SOL_sol bigint ,
+  --SOL_sol bigint ,
 	--EXI_date bigint
 	STR_name varchar ,
 	STR_direction  varchar 
@@ -97,7 +97,7 @@ CREATE TABLE IF NOT EXISTS SOL_TRACK (
 
 CREATE TABLE IF NOT EXISTS SOL_TUNNEL (
 	--STU_id serial ,
-    	--STR_soltrack bigint ,
+  --STR_soltrack bigint ,
 	--EXI_date bigint
 	STU_name varchar ,
 	STU_startlon float8 ,
@@ -116,4 +116,98 @@ CREATE TABLE IF NOT EXISTS EXISTENCE (
 	EXI_start DATE ,
 	EXI_end DATE 
 
+);
+
+
+
+
+
+CREATE TABLE "PARAMETER" (
+	"PAR_id" serial NOT NULL,
+	"PAR_name" varchar NOT NULL,
+	"PAR_value" varchar NOT NULL,
+	"PAR_opvalue" varchar,
+	"STR_soltrack" bigint NOT NULL,
+	"STU_soltunnel" bigint NOT NULL,
+	"OTR_optrack" bigint NOT NULL,
+	"OTU_optunnel" bigint NOT NULL,
+	"OPL_opplatfor" bigint NOT NULL,
+	"ISA_isapplicable" bigint NOT NULL,
+	"TCA_trackcat" bigint NOT NULL,
+	"EXI_date" bigint NOT NULL,
+	CONSTRAINT PARAMETER_pk PRIMARY KEY ("PAR_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "PARAMETER_CATEGORY" (
+	"PCA_id" serial NOT NULL,
+	"PAR_parameter" bigint NOT NULL,
+	CONSTRAINT PARAMETER_CATEGORY_pk PRIMARY KEY ("PCA_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "PARAMETER_FIELD" (
+	"PFI_id" serial NOT NULL,
+	"PCA_pcategory" bigint NOT NULL,
+	CONSTRAINT PARAMETER_FIELD_pk PRIMARY KEY ("PFI_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "PARAMETER_CALCULATOR" (
+	"PCL_id" serial NOT NULL,
+	"PCA_pcategory" bigint NOT NULL,
+	CONSTRAINT PARAMETER_CALCULATOR_pk PRIMARY KEY ("PCL_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "TRACK_PARAMETER_RELATION" (
+	"TPR_id" serial NOT NULL,
+	"PCA_pcategory" bigint NOT NULL,
+	"TCAtrackcat" bigint NOT NULL,
+	CONSTRAINT TRACK_PARAMETER_RELATION_pk PRIMARY KEY ("TPR_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "ISAPPLICABLE" (
+	"ISA_id" serial NOT NULL,
+	CONSTRAINT ISAPPLICABLE_pk PRIMARY KEY ("ISA_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "TRACK_CATEGORY" (
+	"TCA_id" serial NOT NULL,
+	CONSTRAINT TRACK_CATEGORY_pk PRIMARY KEY ("TCA_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "LOCATION_POINT" (
+	"LOC_id" serial NOT NULL,
+	"LOC_lon" float8 NOT NULL,
+	"LOC_lat" float8 NOT NULL,
+	"LOC_distance" float8 NOT NULL,
+	"PAR_parameter" bigint NOT NULL,
+	CONSTRAINT LOCATION_POINT_pk PRIMARY KEY ("LOC_id")
+) WITH (
+  OIDS=FALSE
 );
